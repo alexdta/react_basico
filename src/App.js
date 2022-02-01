dde16222c13c2b0b78ed90e72028895144ffefbe
edgar <alexdta@gmail.com>
2022-02-01 14:55:36 -0600

    Llamado a un componente tipo función El componente recibe un parametro  de fecha y la presenta como un <H3>

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+// ================================================================
+//Componente en forma de función
+//Recibe el dato de fecha
+//Retorna un H3 con el dato recibido
+function fecha(fecha_actual) {
+  var mostrarFecha = (
+    <div>
+      <h3>La fecha de hoy es: {fecha_actual}</h3>
+    </div>
+  );
+  return mostrarFecha;
+}
+// ================================================================
+
+
+
 function App() {
+
+  // Parametros enviados a los componentes
+  let _fecha = new Date();
+  // ========================================================
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Aprendiendo <a className="btn btn-primary" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">REACT JS<i className="ms-1 fab fa-react"></i></a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        {
+          //Llamado a una función con parametros
+          fecha(_fecha.toLocaleDateString())
+        }
+       
       </header>
     </div>
   );
