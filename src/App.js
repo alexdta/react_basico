72bb2606cd54fecf68ebf5f4a8b68b7ab558fd05
edgar <alexdta@gmail.com>
2022-02-02 14:05:11 -0600

    Simplificación de la clase eventos por la versión ECMAScript7

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Presy from "./components/Presentacion";
 import PresFnc from "./components/PresentacionFnc";
 import Est from "./components/Estado";
 import RenderIf from "./components/RenderCondicional";
-import Evento from "./components/Eventos";
+import { Eventos, EventosES7 } from "./components/Eventos";
 // ================================================================
 
 // ================================================================
@@ -18,113 +18,116 @@ Retorna un H3 con el dato recibido
 3 versiones
 */
 function Fecha01(fecha_actual) {
-  var mostrarFecha = (
-    <div>
-      <h3>La fecha de hoy es: {fecha_actual}</h3>
-    </div>
-  );
-  return mostrarFecha;
+	var mostrarFecha = (
+		<div>
+			<h3>La fecha de hoy es: {fecha_actual}</h3>
+		</div>
+	);
+	return mostrarFecha;
 }
 function Fecha02(props) {
-  var mostrarFecha = (
-    <div>
-      <h3>La fecha actual es: {props.fecha_actual}</h3>
-    </div>
-  );
-  return mostrarFecha;
+	var mostrarFecha = (
+		<div>
+			<h3>La fecha actual es: {props.fecha_actual}</h3>
+		</div>
+	);
+	return mostrarFecha;
 }
 
 const Fecha03 = (props) => (
-  <div>
-    <h3>Hoy es: {props.fecha_actual}</h3>
-  </div>
+	<div>
+		<h3>Hoy es: {props.fecha_actual}</h3>
+	</div>
 );
 // ================================================================
 
 function App() {
-  // Parametros enviados a los componentes
-  let _fecha = new Date();
+	// Parametros enviados a los componentes
+	let _fecha = new Date();
 
-  let _nombre = "Edgar Chávez M";
-  let _github = "https://github.com/alexdta/";
-  let _conocimientos = ["C#", "Postgresql", "HTML", "JavaScript", "GIT"];
-  // ========================================================
+	let _nombre = "Edgar Chávez M";
+	let _github = "https://github.com/alexdta/";
+	let _conocimientos = ["C#", "Postgresql", "HTML", "JavaScript", "GIT"];
+	// ========================================================
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Aprendiendo{" "}
-          <a
-            className="btn btn-primary"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            REACT JS<i className="ms-1 fab fa-react"></i>
-          </a>
-        </p>
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Aprendiendo{" "}
+					<a
+						className="btn btn-primary"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						REACT JS<i className="ms-1 fab fa-react"></i>
+					</a>
+				</p>
 
-        <section className="componentes">
-          {
-            //Llamando a un componente
-            //Se hace una etiqueta tipo HTML con el nombre del componente <Presy />
-            //Las propiedades de la etiqueta serán los parametros que se le pasen al componente
-          }
-          {/* <Presy
+				<section className="componentes">
+					{
+						//Llamando a un componente
+						//Se hace una etiqueta tipo HTML con el nombre del componente <Presy />
+						//Las propiedades de la etiqueta serán los parametros que se le pasen al componente
+					}
+					{/* <Presy
             nombre={_nombre}
             github={_github}
             conocimientos={_conocimientos}
           /> */}
 
-          <PresFnc
-            nombre={_nombre}
-            github={_github}
-            conocimientos={_conocimientos}
-          />
-        </section>
-        <br />
-        {
-          //Llamado a una función con parametros
-          Fecha01(_fecha.toLocaleDateString())
+					<PresFnc
+						nombre={_nombre}
+						github={_github}
+						conocimientos={_conocimientos}
+					/>
+				</section>
+				<br />
+				{
+					//Llamado a una función con parametros
+					Fecha01(_fecha.toLocaleDateString())
 
-          /* 
+					/* 
           Abajo se aprecia el llamado a 2 funciones que hacen lo mismo pero las funciones
           y la forma de crear el componente es distinta
           */
-        }
-        <Fecha02 fecha_actual={_fecha.toLocaleDateString()} />
-        <Fecha03 fecha_actual={_fecha.toLocaleDateString()} />
+				}
+				<Fecha02 fecha_actual={_fecha.toLocaleDateString()} />
+				<Fecha03 fecha_actual={_fecha.toLocaleDateString()} />
 
-        <hr />
-        {
-          // Llamado a componente con parametros
-          // Se hace una etiqueta tipo HTML con el nombre del componente <FI></FI> o <FI />
-          // Las propiedades de la etiqueta serán los parámetros que se le pasen al componente
-        }
-        <section>
-          <form>
-            <FI etiqueta="Nombre" tipo="text" id="1" requerido="required"></FI>
-            <FI etiqueta="Fecha" tipo="date" id="2" />
-            <FI etiqueta="Edad" tipo="number" id="3" />
-          </form>
-        </section>
-        <hr />
-        <section>
-          <Est></Est>
-        </section>
-        <hr />
-        <section>
-          <RenderIf />
-        </section>
-        <hr />
-        <section>
-          <Evento />
-        </section>
-      </header>
-    </div>
-  );
+				<hr />
+				{
+					// Llamado a componente con parametros
+					// Se hace una etiqueta tipo HTML con el nombre del componente <FI></FI> o <FI />
+					// Las propiedades de la etiqueta serán los parámetros que se le pasen al componente
+				}
+				<section>
+					<form>
+						<FI etiqueta="Nombre" tipo="text" id="1" requerido="required"></FI>
+						<FI etiqueta="Fecha" tipo="date" id="2" />
+						<FI etiqueta="Edad" tipo="number" id="3" />
+					</form>
+				</section>
+				<hr />
+				<section>
+					<Est></Est>
+				</section>
+				<hr />
+				<section>
+					<RenderIf />
+				</section>
+				<hr />
+				<section>
+					<Eventos />
+					<hr />
+					<EventosES7 />
+					<EventosES7 />
+				</section>
+			</header>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Eventos.js
+++ src/components/Eventos.js
@@ -1,43 +1,83 @@
 import React, { Component } from "react";
 
-export default class Eventos extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { contador: 0 };
+export class Eventos extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { contador: 0 };
 
-    // bind para poder usar 'this' dentro de los metodos
-    this.sumar = this.sumar.bind(this);
-    this.restar = this.restar.bind(this);
-  }
+		// bind para poder usar 'this' dentro de los metodos
+		this.sumar = this.sumar.bind(this);
+		this.restar = this.restar.bind(this);
+	}
 
-  sumar() {
-    this.setState({
-      contador: this.state.contador + 1,
-    });
-  }
+	sumar() {
+		this.setState({
+			contador: this.state.contador + 1,
+		});
+	}
 
-  restar() {
-    if (this.state.contador > 0) {
-      this.setState({
-        contador: this.state.contador - 1,
-      });
-    }
-  }
+	restar() {
+		if (this.state.contador > 0) {
+			this.setState({
+				contador: this.state.contador - 1,
+			});
+		}
+	}
 
-  render() {
-    return (
-      <div>
-        <h2>Eventos en Componente de Clase</h2>
-        <h3>{this.state.contador}</h3>
-        <nav>
-          <button className="btn btn-success me-1" onClick={this.sumar}>
-            <i className="far fa-plus-square"></i>
-          </button>
-          <button className="btn btn-danger ms-1" onClick={this.restar}>
-            <i className="far fa-minus-square"></i>
-          </button>
-        </nav>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<h2>Eventos en Componente de Clase</h2>
+				<h3>{this.state.contador}</h3>
+				<nav>
+					<button className="btn btn-success me-1" onClick={this.sumar}>
+						<i className="far fa-plus-square"></i>
+					</button>
+					<button className="btn btn-danger ms-1" onClick={this.restar}>
+						<i className="far fa-minus-square"></i>
+					</button>
+				</nav>
+			</div>
+		);
+	}
+}
+
+// Properties Initializer
+export class EventosES7 extends Component {
+	state = { contador: 0 };
+
+	/* 
+  No es necesario hacer bind, ya que las "arrow function" heredan 
+  el 'this' del contexto donde se encuentran
+  */
+	sumar = (e) => {
+		this.setState({
+			contador: this.state.contador + 1,
+		});
+	};
+
+	restar = (e) => {
+		if (this.state.contador > 0) {
+			this.setState({
+				contador: this.state.contador - 1,
+			});
+		}
+	};
+
+	render() {
+		return (
+			<div>
+				<h2>Eventos en Componente de Clase (ES7)</h2>
+				<h3>{this.state.contador}</h3>
+				<nav>
+					<button className="btn btn-success me-1" onClick={this.sumar}>
+						<i className="far fa-plus-square"></i>
+					</button>
+					<button className="btn btn-danger ms-1" onClick={this.restar}>
+						<i className="far fa-minus-square"></i>
+					</button>
+				</nav>
+			</div>
+		);
+	}
 }
